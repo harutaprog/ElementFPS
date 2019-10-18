1c80bb70a1e49f49638a134cbec753390aa6956c
HarutaProg <haruta.prog1999@gmail.com>
2019-10-18 17:24:16 +0900

    no message

---
+++ Element/Assets/Scripts/BulletBase.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class BulletBase
+{
+    public int BulletValue;
+
+    //弾を撃つ処理
+    public virtual void Shot()
+    {
+
+    }
+
+    //弾丸を増やす処理
+    public void BulletAdd()
+    {
+
+
+    }
+}
